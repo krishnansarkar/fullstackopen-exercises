0f0ea879d12f4de8ea06d69c5eadac742673303a
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-11-21 22:14:57 -0500

    blog-list: extract app from index.js

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,15 +1,6 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const { PORT, MONGODB_URI } = require('./utils/config')
-const blogsRouter = require('./controllers/blog')
+const app = require('./app')
+const config = require('./utils/config')
 
-const app = express()
-
-mongoose.connect(MONGODB_URI, { family: 4 })
-
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
