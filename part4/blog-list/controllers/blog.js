4e3d52905df9f758276e37a09944c42638e597f8
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2025-11-21 22:04:59 -0500

    blog-list: extracted controller from index.js

---
+++ part4/blog-list/controllers/blog.js
@@ -0,0 +1,27 @@
+const blogRouter = require('express').Router()
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+blogRouter.get('/', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+blogRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+module.exports = blogRouter
--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,35 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const { PORT, MONGODB_URI } = require('./utils/config')
+const blogsRouter = require('./controllers/blog')
 
 const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 mongoose.connect(MONGODB_URI, { family: 4 })
 
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
